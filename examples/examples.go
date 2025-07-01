0c5438662c5dd851a1653678f4a52ec9810cbf00
mrz1836 <mrz1818@gmail.com>
2025-07-01 18:45:06 -0400

    feat: added an example

--- examples/examples.go
+++ examples/examples.go
@@ -1,4 +1,19 @@
-// Package examples provides a collection of example functions
-package examples
+// Package main provides examples for the safe conversion package.
+package main
 
-// TODO - Add example functions that demonstrate the usage of the safe conversion package
+import (
+	"log"
+
+	safeconversion "github.com/bsv-blockchain/go-safe-conversion"
+)
+
+// main function demonstrates the usage of safe conversion functions.
+func main() {
+
+	// Example of converting int32 to uint32 safely
+	val, err := safeconversion.Int32ToUint32(42)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Converted value: %d", val)
+}
